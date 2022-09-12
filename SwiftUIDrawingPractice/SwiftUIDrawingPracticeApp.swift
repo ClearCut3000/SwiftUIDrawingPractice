2bb53c678b389092fdbcf1b3273436c6df3504a9
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-09-12 22:10:32 +0300

    Initial Commit

---
+++ SwiftUIDrawingPractice/SwiftUIDrawingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDrawingPracticeApp.swift
+//  SwiftUIDrawingPractice
+//
+//  Created by Николай Никитин on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDrawingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
