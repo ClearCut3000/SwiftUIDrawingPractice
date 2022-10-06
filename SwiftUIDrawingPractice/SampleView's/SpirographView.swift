2bb6c529416fc4c98e58bb7a0ec6bd7ac7483e2d
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-06 22:16:49 +0300

    Added epitrochoid and icons

--- SwiftUIDrawingPractice/SampleView's/SpirographView.swift
+++ SwiftUIDrawingPractice/SampleView's/SpirographView.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
+enum RouletteType: String, CaseIterable, Identifiable {
+  /// The classic Spirograph toy traces out hypotrochoid and epitrochoid curves ))
+  case hypotrochoid
+  case hypocycloid
+  case epitrochoid
+  var id: RouletteType { self }
+}
+
 struct Spirograph: Shape {
 
   var innerRadius: Int
   var outerRadius: Int
   var distance: Int
   var amount: Double
+  var type: RouletteType
 
-  // Euclid's algorithm
+  /// Euclid's algorithm
   func greatestCommonDivisor(_ a: Int, _ b: Int) -> Int {
     var a = a
     var b = b
@@ -33,6 +42,7 @@ struct Spirograph: Shape {
     let distance = Double(self.distance)
 
     let difference = innerRadius - outerRadius
+    let joint = innerRadius + outerRadius
 
     let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
 
@@ -40,13 +50,24 @@ struct Spirograph: Shape {
     var path = Path()
 
     for theta in stride(from: 0, through: endPoint, by: 0.01) {
-      // Hypotrochoid equations
-//      var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
-//      var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
 
-      // Hypocycloid equations
-      var x = difference * cos(theta) + innerRadius * cos(difference / innerRadius * theta)
-      var y = difference * sin(theta) - innerRadius * sin(difference / innerRadius * theta)
+      var x = 0.0
+      var y = 0.0
+
+      switch type {
+      case .hypotrochoid:
+        /// Hypotrochoid equations
+        x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+        y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+      case .hypocycloid:
+        /// Hypocycloid equations
+        x = difference * cos(theta) + outerRadius * cos(difference / outerRadius * theta)
+        y = difference * sin(theta) - outerRadius * sin(difference / outerRadius * theta)
+      case .epitrochoid:
+        /// Epitrochoid equations
+        x = joint * cos(theta) - distance * cos(joint / outerRadius * theta)
+        y = joint * sin(theta) - distance * sin(joint / outerRadius * theta)
+      }
 
       x += rect.width / 2
       y += rect.height / 2
@@ -69,6 +90,7 @@ struct SpirographView: View {
   @State private var distance = 25.0
   @State private var amount = 1.0
   @State private var hue = 0.6
+  @State private var selectedType = RouletteType.hypocycloid
 
   //MARK: - View Body
   var body: some View {
@@ -80,24 +102,36 @@ struct SpirographView: View {
         Spirograph(innerRadius: Int(innerRadius),
                    outerRadius: Int(outerRadius),
                    distance: Int(distance),
-                   amount: amount)
+                   amount: amount,
+                   type: selectedType)
           .stroke(Color(hue: hue,
                         saturation: 1,
                         brightness: 1), lineWidth: 1)
           .frame(width: 300, height: 300)
-        Spacer()
+
+        Picker("Select roulette type", selection: $selectedType) {
+          ForEach(RouletteType.allCases) { type in
+            Text(type.rawValue.capitalized)
+          }
+        }
+        .pickerStyle(.segmented)
+        .padding()
+
         Group {
+
           Text("Inner Radius: \(Int(innerRadius))")
           Slider(value: $innerRadius, in: 10...150, step: 1)
             .padding([.horizontal, .bottom])
-          
+
           Text("Outer Radius: \(Int(outerRadius))")
           Slider(value: $outerRadius, in: 10...150, step: 1)
             .padding([.horizontal, .bottom])
 
-          Text("Distance: \(Int(distance))")
-          Slider(value: $distance, in: 1...150, step: 1)
-            .padding([.horizontal, .bottom])
+          if selectedType != .hypocycloid {
+            Text("Distance: \(Int(distance))")
+            Slider(value: $distance, in: 1...150, step: 1)
+              .padding([.horizontal, .bottom])
+          }
 
           Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
           Slider(value: $amount)
